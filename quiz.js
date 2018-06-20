ff11edb61aab137863b2092fad869c0fffb66021
Jesse Lopez <Jlopez20515@gmail.com>
2018-06-20 09:23:32 -0700

    finished questions and answer rank

--- quiz.js
+++ quiz.js
@@ -3,7 +3,7 @@ var correct = 0;
 
 // ask questions
 var answer1 = prompt("Name a programming language that's also a gem");
-if ( answer1.toUpperCase() === 'RUBY') {
+if (answer1.toUpperCase() === 'RUBY') {
   correct += 1;
 }
 var answer2 = prompt("Name a programming language that's also a snake");
@@ -19,6 +19,20 @@ if (answer4.toUpperCase() === 'HTML') {
   correct += 1;
 }
 var answer5 = prompt("what language do you use to add interactivity to a web page?");
-if (answer5.toUpperCase === 'JAVASCRIPT') {
+if (answer5.toUpperCase() === 'JAVASCRIPT') {
   correct += 1;
+}
+
+// output results
+document.write("<p>You got " + correct + " out of 5 questions correct.</p>");
+
+// output rank
+if ( correct === 5) {
+  document.write("<p><strong>You earned a gold crown!</strong></p>");
+} else if ( correct >= 3 ) {
+  document.write("<p><strong>You earned a silver crown.</strong></p>");
+} else if (correct >= 1 ) {
+  document.write("<p><strong>You earned a bronze crown.<strong></p>");
+} else {
+  document.write("<p><strong>No crown for you. You need to study.</strong></p>");
 }
